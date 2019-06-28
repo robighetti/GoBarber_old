fd3b1da7d4886ac392facb98dddac2bb9c104432
robighetti <robighetti@gmail.com>
2019-06-28 00:08:58 -0300

    model user criado

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
-  database: 'gobarber',
+  database: 'goBarber',
   define: {
     timestamps: true,
     underscored: true,
