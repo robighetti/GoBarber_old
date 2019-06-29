987aa66bcad2a4ff2c37dd909d0b35882e1f6175
robighetti <robighetti@gmail.com>
2019-06-28 23:35:09 -0300

    nova aula de autenticação

--- src/database/migrations/20190628021348-create-users.js
+++ src/database/migrations/20190628021348-create-users.js
@@ -20,7 +20,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      provide: {
+      provider: {
         type: Sequelize.BOOLEAN,
         defaultValue: false,
         allowNull: false,
